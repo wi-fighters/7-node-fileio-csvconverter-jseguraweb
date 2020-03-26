10e24c3bbeb3cb630e3d75bf45663f2b2abd2f07
Jaime Segura <jseguraweb@gmail.com>
2020-03-26 14:25:44 +0100

    improve solution

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 let csvToJson = require('convert-csv-to-json');
 const path = require('path');
 
-let fileInputName = path.resolve(__dirname, 'demo.csv');
-let fileOutputName = path.resolve(path.basename(fileInputName).split('.')[0] + '.json');
+const [...file] = process.argv.splice(2)
 
-csvToJson.fieldDelimiter(',').getJsonFromCsv(fileInputName);
-csvToJson.generateJsonFileFromCsv(fileInputName, fileOutputName);
+const convertToJSON = (...args) => {
+    if (file.length === 0) {
+        console.log('Please provide a csv file to convert to JSON');
+    } else {
+        let fileInputName = path.resolve(file[0]);
+        let fileOutputName;
+
+        if (file.length === 1) {
+            fileOutputName = path.resolve(path.basename(fileInputName).split('.')[0] + '.json');
+            csvToJson.fieldDelimiter(',').getJsonFromCsv(fileInputName);
+            csvToJson.generateJsonFileFromCsv(fileInputName, fileOutputName);
+            console.log(`JSON file saved as: ${path.basename(fileOutputName)}`);
+        } else if (file.length === 2) {
+            fileOutputName = path.resolve(file[1] + '.json');
+            csvToJson.fieldDelimiter(',').getJsonFromCsv(fileInputName);
+            csvToJson.generateJsonFileFromCsv(fileInputName, fileOutputName);
+            console.log(`JSON file saved as: ${path.basename(fileOutputName)}`);
+        }
+    }
+}
+
+convertToJSON(...file)
